d88adea6eeef4ca97de7ff2e9f1af59665d6ce45
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-06-12 11:45:46 +0100

    User can credit and withdraw from balance

--- spec/bank_spec.js
+++ spec/bank_spec.js
@@ -6,13 +6,28 @@ describe('Bank', function() {
 
   describe('balance', function() {
     it('returns the balance of 0', function() {
-      expect(bank._balance).toEqual(0);
+      expect(bank.balance).toEqual(0);
     });
   });
 
   describe('date', function() {
     it('returns the current date', function() {
-      expect(bank._date).toEqual(today)
+      expect(bank.date).toEqual(today);
     });
   });
+
+  describe('credit', function() {
+    it('returns new balance when money is credited', function() {
+      bank.credit(500);
+      expect(bank.balance).toEqual(500);
+    });
+  });
+
+  describe('withdraw', function() {
+    it('deducts the withdrawn amount from balance', function() {
+      bank.credit(1000);
+      bank.withdraw(90);
+      expect(bank.balance).toEqual(910);
+    })
+  })
 });
--- src/bank.js
+++ src/bank.js
@@ -1,4 +1,12 @@
 function Bank() {
-  this._balance = 0;
-  this._date = today
+  this.date = today
+  this.balance = 0;
+};
+
+Bank.prototype.credit = function (money_in) {
+  this.balance += money_in;
+};
+
+Bank.prototype.withdraw = function (money_out) {
+  this.balance -= money_out;
 };
