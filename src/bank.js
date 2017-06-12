f8a79ac4e1d17f4621d207e1696bada456004fb1
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-06-12 17:01:26 +0100

    Refactored transaction

--- src/bank.js
+++ src/bank.js
@@ -4,18 +4,3 @@
   };
   exports.Bank = Bank;
 })(this);
-// Bank.prototype.credit = function(money_in) {
-//   this.balance += money_in;
-//   var credit_transaction = [this.date, money_in, 0, this.balance]
-//   this.transaction.push(credit_transaction);
-// };
-//
-// Bank.prototype.withdraw = function(money_out) {
-//   this.balance -= money_out;
-//   var withdraw_trans = [this.date, 0, money_out, this.balance]
-//   this.transaction.push(withdraw_trans);
-// };
-//
-// Bank.prototype.view_statement = function() {
-//   return this.transaction.join(" \n");
-// };
--- src/transaction.js
+++ src/transaction.js
@@ -1,17 +1,16 @@
 (function(exports) {
   function Transaction(bank) {
     this.bank = bank;
+    statement = new BankStatement();
   };
 
   Transaction.prototype.credit = function(moneyIn) {
     this.bank.balance += moneyIn;
-    statement = new BankStatement();
     creditTrans = [CurrentDate(), moneyIn, 0, this.bank.balance]
   };
 
   Transaction.prototype.debit = function(moneyOut) {
     this.bank.balance -= moneyOut;
-    statement = new BankStatement();
     debitTrans = [CurrentDate(), 0, moneyOut, this.bank.balance]
   };
   exports.Transaction = Transaction;
